1731c834f32e02a032c76e973ed9fbd6102ac989
mcngnt <mcngnt@MartinDell>
2023-12-03 18:14:09 +0100

    Enable more than 32 static var + change position incr_flag

--- _build/default/ccompile.ml
+++ _build/default/ccompile.ml
@@ -5,6 +5,11 @@ exception Error of string
 
 let hexstring_of_int a = Printf.sprintf "x%x" a
 
+(* 
+Current limitations:
+- No true function call (only memoryless subroutines for now)
+ *)
+
 
 (* Inserts new variable in the table with its name, absolute offest relative to base and scope *)
 let insert_var_tab tab s r isglob = SVAR(s,r, isglob)::tab
@@ -19,8 +24,12 @@ let insert_fun_tab tab s vl =
 								| _ -> raise (Error("Function defined in another."))
 							end
 	in
+	let rec convert_args l i = match l with
+		| [] -> []
+		| h::t -> SVAR(s,i,false)::(convert_args l (i-1))
+	in
 	let sl = convert_decl vl in 
-	SFUN(s,sl)::tab
+	SFUN(s,sl)::( (convert_args sl (-3)) @ tab)
 
 (* 
 let rec get_args tab sx = match tab with
@@ -64,6 +73,12 @@ let print_mult_fun () = "FUN_MULT\nADD R0 R0 #0\nBRn MULT_CHANGE_SIGN\nBR MULT_I
 let print_div_fun () = "FUN_DIV\nAND R2 R2 #0 ; Q\nLD R2 DIV_ISNEG ; Set is_neg to 0\nADD R1 R1 #0\nBRn DIV_A_NEG\nBR DIV_A_POS\nDIV_A_NEG\nNOT R1 R1 ; Change A sign when negative\nADD R1 R1 #1\nADD R0 R0 #0\nBRn DIV_AN_BN\nBR DIV_AN_BP\nDIV_A_POS\nADD R0 R0 #0\nBRn DIV_AP_BN\nBR DIV_AP_BP\nDIV_AN_BN\nNOT R0 R0\nADD R0 R0 #1\nBR DIV_POS\nDIV_AN_BP\nBR DIV_NEG\nDIV_AP_BN\nNOT R0 R0\nADD R0 R0 #1\nBR DIV_NEG\nDIV_AP_BP\nBR DIV_POS\nDIV_NEG\nADD R3 R2 #1\nST R3 DIV_ISNEG ; IS_NEG <- 1\nBR DIV_ENDSIGN\nDIV_POS\nBR DIV_ENDSIGN\nDIV_ENDSIGN\n; Compute the sign of the result in DIV_ISNEG\nDIV_LOOP\nNOT R3 R1\nADD R3 R3 #1\nADD R3 R3 R0 ; R3 <- R0 - R1 = B - R\nBRp DIV_END_LOOP\nAND R3 R3 #0\nADD R3 R3 R0\nNOT R3 R3\nADD R3 R3 #1 ; R3 <- -B\nADD R1 R1 R3 ; R <- R - B\nADD R2 R2 #1\nBR DIV_LOOP\nDIV_END_LOOP\nADD R0 R2 #0\nLD R3 DIV_ISNEG\nBRz DIV_END\nNOT R0 R0\nADD R0 R0 #1\nDIV_END\nRET\nDIV_ISNEG .BLKW #1\n"
 let print_mod_fun () = "FUN_MOD\nAND R2 R2 #0 ; Q\nADD R1 R1 #0\nBRn MOD_A_NEG\nBR MOD_ENDSIGN\nMOD_A_NEG\nADD R1 R1 R0\nBRn MOD_A_NEG\nMOD_ENDSIGN\n; Compute the sign of the result in MOD_ISNEG\nMOD_LOOP\nNOT R3 R1\nADD R3 R3 #1\nADD R3 R3 R0 ; R3 <- R0 - R1 = B - R\nBRp MOD_END_LOOP\nAND R3 R3 #0\nADD R3 R3 R0\nNOT R3 R3\nADD R3 R3 #1 ; R3 <- -B\nADD R1 R1 R3 ; R <- R - B\nADD R2 R2 #1\nBR MOD_LOOP\nMOD_END_LOOP\nADD R0 R1 #0\nMOD_END\nRET\n"
 
+
+let rec forget_comments li = match li with
+			| [] -> []
+			| h::t when h = ";" -> []
+			| h::t -> h::(forget_comments t)
+
 (* Useful for BR to negative conditions *)
 let inverse_condition s = match s with
 	| "z" -> "np"
@@ -74,18 +89,6 @@ let inverse_condition s = match s with
 	| "np" -> "z"
 	| _ -> raise (Error("Can't find an inverse condition for condition : " ^ s))
 
-(* Generates an if e then ctrue else cfalse statement where is treated differently based on the brtype flag *)
-let gen_condition e ctrue cfalse id flagname brtype = let idstring = string_of_int id in
-	let condjmp = "BR" ^ (inverse_condition brtype) ^ " IGNORE_JMP" ^ idstring ^ "\n" ^ "GLEA R3 " ^ flagname ^ "_" ^ "ELSE" ^ idstring ^ "\nJMP R3\n" ^ "IGNORE_JMP" ^ idstring ^ "\n" in
-	e ^ condjmp ^ ctrue ^ "GLEA R3 " ^ flagname ^ "_" ^ "ENDELSE" ^ idstring ^ "\nJMP R3\n" ^ flagname ^ "_" ^  "ELSE" ^ idstring ^ "\n" ^ cfalse ^ flagname ^ "_" ^  "ENDELSE" ^ idstring ^ "\n"
-
-(* Puts the value of a variable in R0 *)
-let get_var a isglob = 
-	(if isglob then "LDR R0 R4 #" ^ (string_of_int a) ^ "\n" else "LDR R0 R5 #-" ^ (string_of_int a) ^ "\n")
-
-(* Sets the value of a variable to the content of R0 *)
-let set_var a isglob =
-	(if isglob then "STR R0 R4 #" ^ (string_of_int a) else "STR R0 R5 #-" ^ (string_of_int a) ) ^ "\n"
 
 (* Base function used to handle the code generation *)
 let check_file f =
@@ -101,10 +104,37 @@ let check_file f =
   let incr_flag () = flagcount := !flagcount + 1 in
   let incr_string () = stringcount := !stringcount + 1 in
 
+  (* Generates an if e then ctrue else cfalse statement where is treated differently based on the brtype flag *)
+  let gen_condition e ctrue cfalse flagname brtype = 
+  	incr_flag();
+  	let idstring = string_of_int !flagcount in
+  	let condjmp = "BR" ^ (inverse_condition brtype) ^ " IGNORE_JMP" ^ idstring ^ "\n" ^ "GLEA R3 " ^ flagname ^ "_" ^ "ELSE" ^ idstring ^ "\nJMP R3\n" ^ "IGNORE_JMP" ^ idstring ^ "\n" in
+  	e ^ condjmp ^ ctrue ^ "GLEA R3 " ^ flagname ^ "_" ^ "ENDELSE" ^ idstring ^ "\nJMP R3\n" ^ flagname ^ "_" ^  "ELSE" ^ idstring ^ "\n" ^ cfalse ^ flagname ^ "_" ^  "ENDELSE" ^ idstring ^ "\n"
+  in
+
+
+  (* Puts the value of a variable in R0 by first storing the address in R1 *)
+  (* I store the address in R1 because if I did LDR R0 R4 #offest, I would be limited to 32 offset, 
+  limiting the number of static variables to 32 *)
+  let get_var a isglob inR0 = 
+  	incr_flag();
+  	"LD R1 CST" ^ (string_of_int !flagcount) ^ "\n" ^ (if isglob then "ADD R1 R4 R1\nLDR R1 R1 #0\n" else "NOT R1 R1\nADD R1 R1 #1\nADD R1 R5 R1\nLDR R1 R1 #0\n") ^ (if inR0 then "ADD R0 R1 #0\n" else "") ^ (print_cst_fill !flagcount a)
+  in
+
+  (* Sets the value of a variable to the content of R0 by first storing the address in R1 *)
+  let set_var a isglob =
+  	incr_flag();
+  	"LD R1 CST" ^ (string_of_int !flagcount) ^ "\n" ^ (if isglob then "ADD R1 R4 R1\nSTR R0 R1 #0\n" else "NOT R1 R1\nADD R1 R1 #1\nADD R1 R5 R1\nSTR R0 R1 #0\n") ^ (print_cst_fill !flagcount a)
+  in
+
+
   (* Puts in LVALUE_ADDR the real address of the variable based on its scope and offset *)
   let set_lvalue addr isglob = 
+  	incr_flag();
   	"LD R0 CST" ^ (string_of_int !flagcount) ^ "\n" ^ ( if isglob then "ADD R0 R0 R4\n" else "NOT R0 R0\nADD R0 R0 #1\nADD R0 R0 R5\n" ) ^ "STR R0 R4 #-1\n" ^ (print_cst_fill !flagcount addr)
   in
+
+
 
   (* Compute the second pass of compilation, translating made up instruction like GLEA to real LC3 code *)
   let secondpass asm = 
@@ -114,12 +144,6 @@ let check_file f =
 			| [] -> raise (Error("Label " ^ sx ^ " not defined."))
 			| (s,n)::t when s = sx -> n
 			| h::t -> find_line sx t
-		in
-
-		let rec forget_comments li = match li with
-			| [] -> []
-			| h::t when h = ";" -> []
-			| h::t -> h::(forget_comments t)
 		in
 
 		(* Stores every label and string declaration with their line number in the code *)
@@ -143,11 +167,11 @@ let check_file f =
 		let rec print_env env = match env with
 			| [] -> ""
 			| (s,c)::t when s = "STATIC_VAR" -> "; mem " ^ (string_of_int c) ^ " " ^ (print_env t)
-			| (s,c)::t when s = ".END" -> (string_of_int c)
+			| (s,c)::t when s = ".END" -> (string_of_int (c-1))
 			| (s,c)::t -> (print_env t)
 		in
 
-		(* Replace GLEA, an instruction used for accessing labels and strings more than 256 lines away, with real LC3 code *)
+		(* Replaces GLEA, an instruction used for accessing labels and strings more than 256 lines away, with real LC3 code *)
 		let rec replace_fun l env = match l with
 			| [] -> ""
 			| h::t -> let args = String.split_on_char ' ' h in
@@ -173,12 +197,12 @@ let check_file f =
 
 	let rec handle_expr le tab = match le with | (l,e) -> begin match e with
 																																(* Change the current lvalue address to the variable's address and set R0 to its value*)
-		| VAR s -> let a,isglob = get_addr_tab tab s in incr_flag(); let slvalue = (set_lvalue a isglob) in slvalue ^ (get_var a isglob) (* Set LVALUE_VAR to a and isglob *)
+		| VAR s -> let a,isglob = get_addr_tab tab s in let slvalue = (set_lvalue a isglob) in slvalue ^ (get_var a isglob  true) (* Set LVALUE_VAR to a and isglob *)
 		| CST x -> incr_flag() ; "LD R0 CST" ^ (string_of_int !flagcount) ^ " ; R0 <- cst " ^ (string_of_int x) ^ "\n" ^ (print_cst_fill !flagcount x)
 		| STRING s ->  incr_string(); strings := (s,!stringcount)::!strings ; "GLEA R0 STRING" ^ (string_of_int !stringcount) ^ "\n"
 		| NULLPTR -> "AND R0 R0 #0\n"
-		| SET_VAR(s,le) -> let a,isglob = get_addr_tab tab s in let e = (handle_expr le tab) in e ^ (set_var a isglob)
-		| SET_VAL(s,le) -> let a,isglob = get_addr_tab tab s in let e = (handle_expr le tab) in e ^ (if isglob then "LDR R1 R4 #" ^ (string_of_int a) else "LDR R1 R5 #-" ^ (string_of_int a) ) ^  "\nSTR R0 R1 #0\n"
+		| SET_VAR(s,le) -> let a,isglob = get_addr_tab tab s in let e = (handle_expr le tab) in e ^ (set_var a isglob )
+		| SET_VAL(s,le) -> let a,isglob = get_addr_tab tab s in let e = (handle_expr le tab) in e ^ (get_var a isglob  false) ^  "\nSTR R0 R1 #0\n"
 		(* | CALL(s,lle) -> gen_call s lle tab (List.length lle) 0 *)
 		| CALL(s,lle) -> ""
 		| OP1(op, le) -> let msg = (handle_expr le tab) in
@@ -215,13 +239,12 @@ let check_file f =
 													 let e = "NOT R0 R0\nADD R0 R0 #1 ;  R0 <- -R0\nADD R0 R0 R1 ; Compute R1 - R0 for comparison\n" in
 													 let ctrue = "AND R0 R0 #0\nADD R0 R0 #1 ; If comparison true, set R0 to 1\n" in 
 													 let cfalse = "AND R0 R0 #0 ; If comparison false, set R0 to 0\n" in
-													 incr_flag();
-													 (gen_condition e ctrue cfalse !flagcount "CMP" brtype) ^ "; R0 <-  e1 " ^ begin match op with
+													 (gen_condition e ctrue cfalse "CMP" brtype) ^ "; R0 <-  e1 " ^ begin match op with
 													 																																					 			| C_LT -> ">"
 													 																																					 			| C_LE -> "<"
 													 																																					 			| C_EQ -> "=="
 													 																																					 end ^ " e2\n"
-		| EIF(le1,le2,le3) -> incr_flag(); let e = (handle_expr le1 tab) and c1 = handle_expr le2 tab and c2 = handle_expr le3 tab in gen_condition e c1 c2 !flagcount "IF" "z"
+		| EIF(le1,le2,le3) -> let e = (handle_expr le1 tab) and c1 = handle_expr le2 tab and c2 = handle_expr le3 tab in gen_condition e c1 c2 "IF" "z"
 		| ESEQ(lle) -> List.fold_left (fun acc le -> acc ^ (handle_expr le tab)) "" lle
 	end
 	in
@@ -239,7 +262,7 @@ let check_file f =
 	and handle_code lc tab r6 = match lc with | (l,c) -> begin match c with
 	 			| CBLOCK(vdl, lcl) -> handle_block vdl lcl tab r6
 				| CEXPR le -> handle_expr le tab
-				| CIF(le, lc1,lc2) -> incr_flag(); let e = (handle_expr le tab) in let c1 = (handle_code lc1 tab r6) in let c2 = (handle_code lc2 tab r6) in gen_condition e c1 c2 !flagcount "IF" "z"
+				| CIF(le, lc1,lc2) -> let e = (handle_expr le tab) in let c1 = (handle_code lc1 tab r6) in let c2 = (handle_code lc2 tab r6) in gen_condition e c1 c2 "IF" "z"
 				| CWHILE(le, lc) -> let idstring = string_of_int (!flagcount) in incr_flag(); "STARTWHILE" ^ idstring ^ "\n" ^ (handle_expr le tab) ^ "BRz ENDWHILE" ^ idstring ^ "\n" ^ (handle_code lc tab r6) ^ "BR STARTWHILE" ^ idstring ^ "\n" ^ "ENDWHILE" ^ idstring ^ "\n"
 				| CRETURN (Some le) -> (handle_expr le tab) ^ "LDR R7 R5 #1 ; Restore R7\nLDR R5 R5 #2 ; Restore R5\nRET\n"
 				| _ -> ""
@@ -259,6 +282,7 @@ let check_file f =
 	let codebody = handle_val_dec f [] 0 in
 
 	incr_flag();
+																											(* Init R6 value to the address of the stack *)                      (*Init R5 and R4 and leave space to store LVALUE_ADDR at offset -1*)   (*Jump to main*)
 	let header = ".ORIG " ^ (hexstring_of_int orig) ^ "\nLD R6 CST" ^ (string_of_int !flagcount) ^"\n" ^ (print_cst_fill !flagcount stack) ^ "ADD R5 R6 #0\nGLEA R4 STATIC_VAR\nADD R4 R4 #1\nGLEA R3 FUN_USER_main\nJMP R3\n" in
 	let protoasm = header
 		^ print_mult_fun()
@@ -271,3 +295,12 @@ let check_file f =
 	let finalasm = secondpass protoasm in
 	finalasm
 
+
+
+(* 
+Features :
+		- Can handle more than 32 static variables and local variables at the same time (normally difficult beacause the offset is in the -32,32 range)
+		- Can jump to arbitrarly far away piece of code with the custom instruction GLEA, converted to real LC3 code in a second pass
+		- Division, multiplication and modulo operation implemented in a subroutine to be more lines-efficient
+		- Can handle IF and WHILE statements more than 256 lines long by using GLEA and JMP
+ *)
